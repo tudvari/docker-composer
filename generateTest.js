d129ea5b85df687cfef2a49b7c9a63a74d3d5f24
Tibor Udvari <tudvari@tudvari.com>
2018-12-09 11:00:23 +0100

    input validation with schema

--- generateTest.js
+++ generateTest.js
@@ -3,14 +3,24 @@ const should = require('should')
 const composer = require('./index')
 const YML = require('js-yaml')
 
-describe('composer', function () {
+describe('composer', function ( ) {
 
-	it('Should equal', async function () {
+	it('Should equal', function ( ) {
 
 		let expectedYML = YML.safeLoad(fs.readFileSync('./test_datas/first.yml'), 'utf-8')
-		let result = await composer.generate(JSON.parse(fs.readFileSync('./test_datas/first.json')))
+		let result = composer.generate(JSON.parse(fs.readFileSync('./test_datas/first.json')))
 		let actualYML = YML.safeLoad(result, 'utf-8')
 
 		should.equal(YML.dump(actualYML), YML.dump(expectedYML))
+	})
+
+	it('Validator test - invalid ', function ( ) {
+		try {
+			composer.generate(JSON.parse(fs.readFileSync('./test_datas/invalid_input.json')))
+		}
+		catch ( error ) {
+			should.exists(error)
+			should.equal(error.message, 'Invalid input!')
+		}
 	})
 })
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const fs = require('fs')
 const YAML = require('json2yaml')
+const Validator = require('jsonschema').Validator
+const v = new Validator()
 
 function generateCompose ( inputJSON ) {
-	let result = YAML.stringify( inputJSON )
-	return result
+	let schema = JSON.parse(fs.readFileSync('./schema.json'))
+
+	if ( !v.validate(inputJSON, schema).valid )
+		throw new Error('Invalid input!')
+
+	return YAML.stringify( inputJSON )
 }
 
 module.exports = {
