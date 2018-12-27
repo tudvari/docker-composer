a7223783e2f37006a0a82ce9c59412c2359252b6
Tibor Udvari <tudvari@tudvari.com>
2018-12-27 11:51:10 +0100

    update dependency, gulp task fix, eslint config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,9 @@
-'use strict'
 var gulp = require('gulp')
 var eslint = require('gulp-eslint')
 
-
-gulp.task('lint', function () {
+function lint() {
 	return gulp.src(['index.js'])
 		.pipe(eslint())
 		.pipe(eslint.formatEach())
-})
-
-gulp.task('default', ['lint'], function () {
-	console.log('default task is running')
-})
+}
+exports.default = lint
