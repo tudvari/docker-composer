b2f8cb985b511dc41daf35d83e0fa34dd9b15b3b
tudvari <tudvari@tudvari.com>
2022-12-23 13:25:44 +0100

    moving tests to tests folder

--- tests/generateTest.js
+++ tests/generateTest.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const should = require('should')
-const composer = require('./index')
+const composer = require('../index')
 const YML = require('js-yaml')
 
 describe('composer', function ( ) {
 
 	it('Should equal', function ( ) {
 
-		let expectedYML = YML.load(fs.readFileSync('./test_datas/first.yml'), 'utf-8')
-		let result = composer.generate(JSON.parse(fs.readFileSync('./test_datas/first.json')))
+		let expectedYML = YML.load(fs.readFileSync('./tests/test_datas/first.yml'), 'utf-8')
+		let result = composer.generate(JSON.parse(fs.readFileSync('./tests/test_datas/first.json')))
 		let actualYML = YML.load(result, 'utf-8')
 
 		should.equal(YML.dump(actualYML), YML.dump(expectedYML))
@@ -16,7 +16,7 @@ describe('composer', function ( ) {
 
 	it('Validator test - invalid ', function ( ) {
 		try {
-			composer.generate(JSON.parse(fs.readFileSync('./test_datas/invalid_input.json')))
+			composer.generate(JSON.parse(fs.readFileSync('./tests/test_datas/invalid_input.json')))
 		}
 		catch ( error ) {
 			should.exists(error)
