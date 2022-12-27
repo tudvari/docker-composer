6dabe70a65551f98c65c5082e72bff6debbabe85
tudvari <tudvari@tudvari.com>
2022-12-27 12:10:41 +0100

    relate path usage

--- scripts/compile-ts-types.js
+++ scripts/compile-ts-types.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 const compiler = require('json-schema-to-typescript');
 
 /**
  * Helper function to generate typescript types from json schema
  */
 async function runner() {
-  const compiledTs = await compiler.compileFromFile('schema.json');
+  const compiledTs = await compiler.compileFromFile(
+      path.resolve(__dirname, '../schema.json'));
   fs.writeFileSync('index.d.ts', compiledTs);
   console.log('Types are generated..');
 }
